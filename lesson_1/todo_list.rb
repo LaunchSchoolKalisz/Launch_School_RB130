858255c2c27750b278747a43a4f55e04835be393
mskalisz <mskalisz3@gmail.com>
2022-01-22 13:55:00 -0500

    Add tests for todo list methods

--- lesson_1/todo_list.rb
+++ lesson_1/todo_list.rb
@@ -219,3 +219,22 @@ list.to_s                      # returns string representation of the list
 # # [ ] Buy milk
 # # [X] Clean room
 # # [ ] Go to gym
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+puts list
+
+list.pop
+
+puts list
+
+list.mark_done_at(1)
+
+puts list
