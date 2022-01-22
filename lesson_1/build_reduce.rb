0c19ef9b67a0033a04505b897cbb3458c89fbf6f
mskalisz <mskalisz3@gmail.com>
2022-01-22 11:15:06 -0500

    Add solution for build reduce method

--- lesson_1/build_reduce.rb
+++ lesson_1/build_reduce.rb
@@ -5,8 +5,15 @@ operate on. It should also take an optional second argument that represents the 
 accumulator.
 =end
 
+def reduce(array, accumulator = 0)
+  array.each do |num|
+    accumulator = yield(accumulator, num)
+  end
+  accumulator
+end
+
 array = [1, 2, 3, 4, 5]
 
-reduce(array) { |acc, num| acc + num }                    # => 15
-reduce(array, 10) { |acc, num| acc + num }                # => 25
-reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p reduce(array) { |acc, num| acc + num }                    # => 15
+p reduce(array, 10) { |acc, num| acc + num }                # => 25
+p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
