a1d6a85ea1f6cee1dcf34df3b38bd750a671f8b9
mskalisz <mskalisz3@gmail.com>
2022-01-22 14:02:38 -0500

    Add method calls

--- lesson_1/todo_#each.rb
+++ lesson_1/todo_#each.rb
@@ -124,3 +124,16 @@ class TodoList
     text
   end
 end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+list.each do |todo|
+  puts todo                   # calls Todo#to_s
+end
