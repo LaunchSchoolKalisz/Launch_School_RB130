607ebf873172ea495315384b07f87ad65c5c5e4e
mskalisz <mskalisz3@gmail.com>
2022-01-22 10:54:00 -0500

    Add LS solution for building a select method

--- lesson_1/build_select.rb
+++ lesson_1/build_select.rb
@@ -21,3 +21,23 @@ array = [1, 2, 3, 4, 5]
 p select(array) { |num| num.odd? }      
 p select(array) { |num| puts num }      
 p select(array) { |num| num + 1 }
+
+=begin
+LS Solution
+
+def select(array)
+  counter = 0
+  result = []
+
+  while counter < array.size
+    current_element = array[counter]
+    result << current_element if yield(current_element)
+    counter += 1
+  end
+
+  result
+end
+
+Note that we used a while loop here, but since we're expecting an Array object as the parameter, 
+we could have as well used the Array#each to iterate.
+=end
