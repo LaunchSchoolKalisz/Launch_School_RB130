2a0851695ee67543851965cfaa532ca783417c70
mskalisz <mskalisz3@gmail.com>
2022-01-23 12:33:51 -0500

    Add LS solution for divisors

--- lesson_2/easy_1/4_divisors.rb
+++ lesson_2/easy_1/4_divisors.rb
@@ -28,3 +28,28 @@ p divisors(7) == [1, 7]
 p divisors(12) == [1, 2, 3, 4, 6, 12]
 p divisors(98) == [1, 2, 7, 14, 49, 98]
 p divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
+
+=begin
+LS solution
+
+def divisors(number)
+  1.upto(number).select do |candidate|
+    number % candidate == 0
+  end
+end
+
+Discussion
+The solution is straightforward; it simply iterates through all of the candidate divisors, and creates a 
+list of the actual divisors.
+
+Further Exploration
+You may have noticed that the final example took a few seconds, maybe even a minute or more, to complete. 
+This shouldn't be a surprise as we aren't doing anything to optimize the algorithm. It's what is commonly 
+called a brute force algorithm where you try every possible solution; these are easy to write, but they 
+don't always produce fast results. They aren't necessarily bad solutions -- it depends on your needs -- 
+but the speed of brute force algorithms should always be examined.
+
+How fast can you make your solution go? How big of a number can you quickly reduce to its divisors? Can you 
+make divisors(999962000357) return almost instantly? (The divisors are [1, 999979, 999983, 999962000357].) 
+Hint: half of the divisors gives you the other half.
+=end
