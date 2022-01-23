1030bc478552b52200f061c428c05ef0b13fdb4f
mskalisz <mskalisz3@gmail.com>
2022-01-23 12:19:47 -0500

    Solve find missing numbers

--- lesson_2/easy_1/3_find_missing_numbers.rb
+++ lesson_2/easy_1/3_find_missing_numbers.rb
@@ -9,4 +9,17 @@ missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
 missing([1, 2, 3, 4]) == []
 missing([1, 5]) == [2, 3, 4]
 missing([6]) == []
+
+# Problem: Create a method which accepts 1 argument. Return an array with missing integers between first
+and last integers
 =end
+
+def missing(missing_array)
+  full_array = (missing_array[0]..missing_array[-1]).to_a
+  return_array = full_array - missing_array
+end
+
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []
