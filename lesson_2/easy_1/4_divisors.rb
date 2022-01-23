70191baa5dd1cc13c93723ba6af29d1205fe58ae
mskalisz <mskalisz3@gmail.com>
2022-01-23 12:30:20 -0500

    Solve divisors

--- lesson_2/easy_1/4_divisors.rb
+++ lesson_2/easy_1/4_divisors.rb
@@ -9,4 +9,22 @@ divisors(7) == [1, 7]
 divisors(12) == [1, 2, 3, 4, 6, 12]
 divisors(98) == [1, 2, 7, 14, 49, 98]
 divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
+
+# Problem
+- Create a method which takes 1 argument
+- Returns an array with everything which devides equally
 =end
+
+def divisors(num)
+  results = []
+  1.upto(num) do |dividand|
+    results << dividand if num % dividand == 0
+  end
+  results
+end
+
+p divisors(1) == [1]
+p divisors(7) == [1, 7]
+p divisors(12) == [1, 2, 3, 4, 6, 12]
+p divisors(98) == [1, 2, 7, 14, 49, 98]
+p divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
