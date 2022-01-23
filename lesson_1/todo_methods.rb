f82be523c25da688802907ea7dd01221a510f1bb
mskalisz <mskalisz3@gmail.com>
2022-01-22 23:48:44 -0500

    Add LS solution for extra methods

--- lesson_1/todo_methods.rb
+++ lesson_1/todo_methods.rb
@@ -194,4 +194,35 @@ list.mark_all_undone
 list.mark_all_undone
 p list
 
+=begin
+LS Solution
 
+class TodoList
+
+  # ... rest of code omitted for brevity
+
+  def find_by_title(title)
+    select { |todo| todo.title == title }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
+end
+=end
