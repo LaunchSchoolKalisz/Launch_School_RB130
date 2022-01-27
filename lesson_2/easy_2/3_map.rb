c14dc9b0867326dc6ad2cda66f22a1a647029428
mskalisz <mskalisz3@gmail.com>
2022-01-27 15:31:08 -0500

    Solve map

--- lesson_2/easy_2/3_map.rb
+++ lesson_2/easy_2/3_map.rb
@@ -27,3 +27,17 @@ map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
 map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
 map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
 =end
+
+def map(array)
+  new_array = []
+  array.each do |element|
+    new_array << yield(element)
+  end
+  new_array
+end
+
+p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+p map([]) { |value| true } == []
+p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
