82792c7f921983cd04970c8e187e52d55c57bba5
mskalisz <mskalisz3@gmail.com>
2022-01-27 13:53:10 -0500

    Add solution for count items

--- lesson_2/easy_1/10_count_items.rb
+++ lesson_2/easy_1/10_count_items.rb
@@ -14,3 +14,14 @@ count([1,2,3,4,5]) { |value| false } == 0
 count([]) { |value| value.even? } == 0
 count(%w(Four score and seven)) { |value| value.size == 5 } == 2
 =end
+
+def count(array)
+  array.select {|value| yield(value)}.count
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
