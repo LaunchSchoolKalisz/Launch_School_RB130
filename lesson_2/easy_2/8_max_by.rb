b6151a5c3db142fc75f94876b3ff2e46f73fa065
mskalisz <mskalisz3@gmail.com>
2022-01-29 17:26:48 -0500

    Solve max by

--- lesson_2/easy_2/8_max_by.rb
+++ lesson_2/easy_2/8_max_by.rb
@@ -21,3 +21,24 @@ max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5]
 max_by([-7]) { |value| value * 3 } == -7
 max_by([]) { |value| value + 5 } == nil
 =end
+
+def max_by(array)
+  current_largest_element = nil
+  array.each do |element|
+    if current_largest_element == nil
+      current_largest_element = element
+    else
+      if yield(element) > yield(current_largest_element)
+        current_largest_element = element
+      end
+    end
+  end
+  current_largest_element
+end
+
+p max_by([1, 5, 3]) { |value| value + 2 } == 5
+p max_by([1, 5, 3]) { |value| 9 - value } == 1
+p max_by([1, 5, 3]) { |value| (96 - value).chr } == 1
+p max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5]
+p max_by([-7]) { |value| value * 3 } == -7
+p max_by([]) { |value| value + 5 } == nil
