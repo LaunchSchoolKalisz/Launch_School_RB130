21f3f8eb42b5e99838240b321ccbeb29e858dcac
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:53:22 -0500

    Add prompt for type assertions

---
+++ lesson_2/easy_testing/type_assertions.rb
@@ -0,0 +1,6 @@
+=begin
+Type Assertions
+
+Write a minitest assertion that will fail if value is not an instance of the Numeric class exactly. 
+value may not be an instance of one of Numeric's superclasses.
+=end
