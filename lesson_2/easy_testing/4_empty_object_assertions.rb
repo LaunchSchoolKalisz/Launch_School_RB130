da2b817070e647223a63abc9b00abefc237aa31b
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:18:09 -0500

    Add LS solution for empty object assertions

--- lesson_2/easy_testing/4_empty_object_assertions.rb
+++ lesson_2/easy_testing/4_empty_object_assertions.rb
@@ -5,3 +5,18 @@ Write a minitest assertion that will fail if the Array list is not empty.
 =end
 
 assert_empty(array)
+
+=begin
+LS Solution
+
+assert_empty list
+
+Discussion
+
+#assert_empty tests whether its first argument responds to the method #empty? with a true value. 
+You can use:
+
+assert_equal true, list.empty?
+
+instead, but #assert_empty is clearer and issues a better failure message.
+=end
