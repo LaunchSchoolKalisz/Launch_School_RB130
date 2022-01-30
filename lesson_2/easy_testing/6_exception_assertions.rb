e79c7fe76f4bf9001486a51c14df467c8af76561
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:52:20 -0500

    Add LS solution for exception assertions

--- lesson_2/easy_testing/6_exception_assertions.rb
+++ lesson_2/easy_testing/6_exception_assertions.rb
@@ -5,3 +5,14 @@ Write a minitest assertion that will fail unless employee.hire raises a NoExperi
 =end
 
 assert_raises(NoExperienceError) {employee.hire}
+
+=begin
+
+assert_raises NoExperienceError do
+  employee.hire
+end
+
+Discussion
+#assert_raises checks if the given block raises an exception of the named type. If it does not, the 
+assertion fails.
+=end
