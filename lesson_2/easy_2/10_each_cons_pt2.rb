1e482314e1bc422342e10c13ff194365f557417b
mskalisz <mskalisz3@gmail.com>
2022-01-30 16:47:41 -0500

    Add LS solution to each_cons part 2

--- lesson_2/easy_2/10_each_cons_pt2.rb
+++ lesson_2/easy_2/10_each_cons_pt2.rb
@@ -44,32 +44,64 @@ end
 hash == {}
 =end
 
+def each_cons(array, n)
+  array.each_index do |index|
+    break if index + n - 1 >= array.size
+    yield(*array[index..(index + n - 1)])
+  end
+  nil
+end
+
 hash = {}
 each_cons([1, 3, 6, 10], 1) do |value|
   hash[value] = true
 end
-hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+p hash #== { 1 => true, 3 => true, 6 => true, 10 => true }
 
 hash = {}
 each_cons([1, 3, 6, 10], 2) do |value1, value2|
   hash[value1] = value2
 end
-hash == { 1 => 3, 3 => 6, 6 => 10 }
+p hash #== { 1 => 3, 3 => 6, 6 => 10 }
 
 hash = {}
 each_cons([1, 3, 6, 10], 3) do |value1, *values|
   hash[value1] = values
 end
-hash == { 1 => [3, 6], 3 => [6, 10] }
+p hash #== { 1 => [3, 6], 3 => [6, 10] }
 
 hash = {}
 each_cons([1, 3, 6, 10], 4) do |value1, *values|
   hash[value1] = values
 end
-hash == { 1 => [3, 6, 10] }
+p hash #== { 1 => [3, 6, 10] }
 
 hash = {}
 each_cons([1, 3, 6, 10], 5) do |value1, *values|
   hash[value1] = values
 end
-hash == {}
+p hash #== {}
+
+=begin
+LS Solution
+
+def each_cons(array, n)
+  array.each_index do |index|
+    break if index + n - 1 >= array.size
+    yield(*array[index..(index + n - 1)])
+  end
+  nil
+end
+
+Discussion
+This exercise would be pretty hard if you hadn't already done the bulk of the work in the previous exercise. 
+Even so, it can be tricky.
+
+As before, you need to know when to stop iterating. This can be handled pretty easily by realizing that the 
+previous exercise just had n == 2, and we stopped processing when index + 1, or index + 2 - 1 was greater 
+than or equal to the Array size. This translates pretty naturally to testing whether index + n - 1 is 
+greater than or equal to the Array size.
+
+Slightly trickier is passing a variable number of elements to the block; we do this using the "splat" 
+operator (*) together with a slice of the array that covers n elements starting at the current index.
+=end
