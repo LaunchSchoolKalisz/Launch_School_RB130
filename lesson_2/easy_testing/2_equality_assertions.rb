2ff8c9f83ae5daf621c3f4fa3b6c7e9fcbd6a6b2
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:07:19 -0500

    Add LS solution for equality assertions

--- lesson_2/easy_testing/2_equality_assertions.rb
+++ lesson_2/easy_testing/2_equality_assertions.rb
@@ -4,3 +4,15 @@ Write a minitest assertion that will fail if value.downcase does not return 'xyz
 =end
 
 assert_equal('xyz', value.downcase)
+
+=begin
+LS Solution
+
+assert_equal 'xyz', value.downcase
+
+Discussion
+#assert_equal tests whether its first two arguments are equal using #==. Failure messages issued by 
+#assert_equal assume that the first argument represents the expected value, while the second argument 
+represents the actual value to be tested. It's important to maintain this convention so that failure 
+messages make sense.
+=end
