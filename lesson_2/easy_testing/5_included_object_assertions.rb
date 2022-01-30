ef5c51bc1788b532a443705377a907c8fdde4266
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:21:26 -0500

    Add LS solution for included object assertions

--- lesson_2/easy_testing/5_included_object_assertions.rb
+++ lesson_2/easy_testing/5_included_object_assertions.rb
@@ -5,3 +5,16 @@ Write a minitest assertion that will fail if the 'xyz' is not in the Array list.
 =end
 
 assert_includes(list, 'xyz')
+
+=begin
+LS Solution
+
+assert_includes list, 'xyz'
+
+Discussion
+#assert_includes tests whether its first argument contains the second argument. You can use:
+
+assert_equal true, list.include?('xyz')
+
+instead, but #assert_includes is clearer and issues a better failure message.
+=end
