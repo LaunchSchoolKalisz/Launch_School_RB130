400a32ecb683b2773a6187c8f08301108a758f67
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:59:40 -0500

    Add LS solution for kind assertions

--- lesson_2/easy_testing/8_kind_assertions.rb
+++ lesson_2/easy_testing/8_kind_assertions.rb
@@ -6,3 +6,13 @@ subclasses (e.g., Integer, Float, etc).
 =end
 
 assert_kind_of(Numeric, value)
+
+=begin
+LS Solution
+
+assert_kind_of Numeric, value
+
+Discussion
+#assert_kind_of uses Object#kind_of? to check if the class of its second argument is an instance of the
+named class or one of the named class's subclasses.
+=end
