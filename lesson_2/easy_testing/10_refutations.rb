1d87d3fd52a9cba41f1e8f2528d882dd0c6ff3d5
mskalisz <mskalisz3@gmail.com>
2022-01-30 18:14:37 -0500

    Add LS solution for refutations

--- lesson_2/easy_testing/10_refutations.rb
+++ lesson_2/easy_testing/10_refutations.rb
@@ -5,3 +5,13 @@ Write a test that will fail if 'xyz' is one of the elements in the Array list:
 =end
 
 refute_includes list, 'xyz'
+
+=begin
+
+refute_includes list, 'xyz'
+
+Discussion
+Most minitest assertions have a corresponding refutation method that fails if the indicated condition is 
+true: refute_equal is the opposite of assert_equal, refute_kind_of is the opposite of assert_kind_of, etc. 
+Thus, the refutation of assert_includes is refute_includes.
+=end
