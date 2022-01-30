b987b4df608f5ed3dfa04b0175fda22da850b293
mskalisz <mskalisz3@gmail.com>
2022-01-30 18:05:06 -0500

    Add LS solution for same object assertions

--- lesson_2/easy_testing/9_same_object_assertions.rb
+++ lesson_2/easy_testing/9_same_object_assertions.rb
@@ -5,3 +5,12 @@ Write a test that will fail if list and the return value of list.process are dif
 =end
 
 assert_match(list, list.process)
+
+=begin
+LS Solution
+
+assert_same list, list.process
+
+Discussion
+#assert_same tests whether its first and second arguments are the same object, as determined by #equal?.
+=end
