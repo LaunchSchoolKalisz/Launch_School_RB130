4e583574cf2bf8a8dde138e2136a16e2f2165446
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:59:01 -0500

    Add solution for kind assertions

--- lesson_2/easy_testing/8_kind_assertions.rb
+++ lesson_2/easy_testing/8_kind_assertions.rb
@@ -4,3 +4,5 @@ Kind Assertions
 Write a minitest assertion that will fail if the class of value is not Numeric or one of Numeric's 
 subclasses (e.g., Integer, Float, etc).
 =end
+
+assert_kind_of(Numeric, value)
