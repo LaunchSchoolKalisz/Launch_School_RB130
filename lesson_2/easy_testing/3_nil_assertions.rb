bd154a704b4f3874fb5c1a9228396a26fb71b334
mskalisz <mskalisz3@gmail.com>
2022-01-30 17:12:44 -0500

    Add LS solution for nil assertions

--- lesson_2/easy_testing/3_nil_assertions.rb
+++ lesson_2/easy_testing/3_nil_assertions.rb
@@ -4,3 +4,17 @@ Write a minitest assertion that will fail if value is not nil.
 =end
 
 assert_equal(nil, value)
+
+=begin
+LS Solution
+
+assert_nil value
+
+Discussion
+
+#assert_nil tests whether its first argument is nil. You can use:
+
+assert_equal nil, value
+
+instead, but #assert_nil is clearer and issues a better failure message.
+=end
