2f6f51578fdbb673e26272799589db273d3a6142
mskalisz <mskalisz3@gmail.com>
2022-01-30 20:39:25 -0500

    Add LS solution to passing parameters part 2

--- lesson_2/medium_1/4_passing_parameters_2.rb
+++ lesson_2/medium_1/4_passing_parameters_2.rb
@@ -38,3 +38,29 @@ grouping(%w(raven finch hawk eagle)) do |array|
   ignore1, ignore2, *raptors = array
   raptors
 end
+
+=begin
+LS Solution
+
+birds = ['crow', 'finch', 'hawk', 'eagle']
+
+def types(birds)
+  yield birds
+end
+
+types(birds) do |_, _, *raptors|
+  puts "Raptors: #{raptors.join(', ')}."
+end
+
+Discussion
+For our solution, we start with an array named birds. It contains four birds, where two of them are 
+raptors. Above we use our block variables to organize it further. When we yield birds to the block, 
+Ruby assigns the individual elements of birds to the different block variables. It assigns the first 
+two birds, "crow" and "finch," to _; the underscore tells Ruby that we aren't interested in those values. 
+The splat operator on the name block variable tells Ruby to treat it as an Array and assign all remaining 
+arguments to it. Here, we group the last two elements from birds into the array, raptors.
+
+For demonstration purposes, we join the two array elements and output them to the screen as:
+
+Raptors: hawk, eagle
+=end
