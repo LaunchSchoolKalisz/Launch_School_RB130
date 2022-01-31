73023bc32983290074496b222f357d9851019f40
mskalisz <mskalisz3@gmail.com>
2022-01-30 20:12:32 -0500

    Add LS solution for passing parameters part 1

--- lesson_2/medium_1/3_passing_parameters.rb
+++ lesson_2/medium_1/3_passing_parameters.rb
@@ -23,3 +23,28 @@ def gather(items)
 end
 
 gather(items) {|items| items.join(', ')}
+
+=begin
+LS Solution
+
+items = ['apples', 'corn', 'cabbage', 'wheat']
+
+def gather(items)
+  puts "Let's start gathering food."
+  yield(items)
+  puts "Nice selection of food we have gathered!"
+end
+
+gather(items) { |produce| puts produce.join('; ') }
+
+Discussion
+To accomplish our task, we need to yield the items array to a block. Moving the implementation of our 
+items formatting to a block gives the user more flexibility and choice for how they wish to use gather. 
+By yielding items to a block, the user can join those items for presentation purposes or they can choose a 
+completely different representation For example:
+
+gather(items) do |produce|
+  puts "We've gathered some vegetables: #{produce[1]} and #{produce[2]}"
+end
+
+=end
