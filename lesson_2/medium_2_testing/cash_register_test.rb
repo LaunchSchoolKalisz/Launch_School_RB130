66b2f574e6db5039b1c9c99a9d704cf3a034e32e
mskalisz <mskalisz3@gmail.com>
2022-02-03 11:19:03 -0500

    Add setup and test for accept_money

--- lesson_2/medium_2_testing/cash_register_test.rb
+++ lesson_2/medium_2_testing/cash_register_test.rb
@@ -7,6 +7,17 @@ require_relative 'transaction'
 
 class CashRegisterTest < MiniTest::Test
   def setup
+    @register = CashRegister.new(100)
+  end
+
+  def test_accept_money
+    transaction = Transaction.new(20)
+    transaction.amount_paid = 20
+
+    previous_amount = @register.total_money
+    @register.accept_money(transaction)
+    current_amount = @register.total_money
+    assert_equal(previous_amount + 20, current_amount)
   end
 end
 
