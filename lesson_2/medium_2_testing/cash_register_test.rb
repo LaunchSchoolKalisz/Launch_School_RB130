61ebb2215d2553837ea9db72a0bf57215d52e8bf
mskalisz <mskalisz3@gmail.com>
2022-02-02 21:28:19 -0500

    Add testing setup code for cash register test

--- lesson_2/medium_2_testing/cash_register_test.rb
+++ lesson_2/medium_2_testing/cash_register_test.rb
@@ -1,3 +1,14 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+class CashRegisterTest < MiniTest::Test
+  def setup
+  end
+end
 
 =begin
 require 'minitest/autorun'
