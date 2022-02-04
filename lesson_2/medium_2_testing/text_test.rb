20eeda6e70750ba8dab26b78126e9fc5007dc334
mskalisz <mskalisz3@gmail.com>
2022-02-04 18:42:50 -0500

    Reformat LS solution for text test

--- lesson_2/medium_2_testing/text_test.rb
+++ lesson_2/medium_2_testing/text_test.rb
@@ -57,12 +57,23 @@ class TextTest < Minitest::Test
     @file.close
   end
 end
+
 Discussion
-To write this test we'll need to first create an instance of class Text. Then, we need to pass it the text we want to work on. To do that we'll have to read that text from the sample text file. First, we must open the file for reading. We want to keep things directly related to our test in our test method. So, any code that is necessary to setup the test is a perfect candidate for the #setup method. We do so in our solution by opening the file in question and assigning the file object to an instance variable.
 
-Once we have passed in the contents of that file to the Text class, we're ready to make the command that will lead to the assertion for this test. That command is the call to Text#swap. In this case, the exercise asks us to use a and e as the letters to swap out. The return value of this Text#swap method is what we will use to test that this method is working as intended.
+To write this test we'll need to first create an instance of class Text. Then, we need to pass it the text 
+we want to work on. To do that we'll have to read that text from the sample text file. First, we must open 
+the file for reading. We want to keep things directly related to our test in our test method. So, any code 
+that is necessary to setup the test is a perfect candidate for the #setup method. We do so in our solution 
+by opening the file in question and assigning the file object to an instance variable.
 
-Next, we need a String that we can test against the return value of Text#swap. This will be our expected value for the later assertion. To get this String, we just take the contents of the file and process it manually with our text editor to replace all of the as with es.
+Once we have passed in the contents of that file to the Text class, we're ready to make the command that 
+will lead to the assertion for this test. That command is the call to Text#swap. In this case, the exercise 
+asks us to use a and e as the letters to swap out. The return value of this Text#swap method is what we 
+will use to test that this method is working as intended.
+
+Next, we need a String that we can test against the return value of Text#swap. This will be our expected 
+value for the later assertion. To get this String, we just take the contents of the file and process it 
+manually with our text editor to replace all of the as with es.
 
 Finally, we use assert_equal to ensure that the expected value of the text is equal to the actual value.
 =end
