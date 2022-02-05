38ec6d325c17282e1f7bf2ab3456057a7e6add17
mskalisz <mskalisz3@gmail.com>
2022-02-05 09:15:56 -0500

    Add LS solution for text test

--- lesson_2/medium_2_testing/text_test.rb
+++ lesson_2/medium_2_testing/text_test.rb
@@ -1,4 +1,6 @@
 require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 require_relative 'text'
 
@@ -28,8 +30,9 @@ class TextTest < MiniTest::Test
     assert_equal(72, counted)
   end
 
-  def takedown
+  def teardown
     @file.close
+    puts "File has been closed: #{@file.closed?}"
   end
 end
 
@@ -82,4 +85,65 @@ value for the later assertion. To get this String, we just take the contents of 
 manually with our text editor to replace all of the as with es.
 
 Finally, we use assert_equal to ensure that the expected value of the text is equal to the actual value.
+
+LS Solution to Test Word Count
+
+require 'minitest/autorun'
+require_relative 'text'
+
+class TextTest < Minitest::Test
+  def setup
+    @file = File.open('./sample_text.txt', 'r')
+  end
+
+  # omitted for brevity
+
+  def test_word_count
+    text = Text.new(@file.read)
+    assert_equal 72, text.word_count
+  end
+
+  def teardown
+    @file.close
+  end
+end
+
+Discussion
+For this test we first need to determine the word count of the sample text. That can be ascertained easily 
+enough by calling the word_count method manually, or by reading in the file in irb and counting the words 
+from there. We then write in that value into our test as the expected value. We also need to make sure that 
+we read the file to gain access to the relevant text. We're able to call @file.read since the opening of 
+our file is handled in the setup method. If we didn't first open that file, then calling @file.read would 
+throw an error. Lastly, we use assert_equal with our hard-coded word count and a call to text.word_count as 
+arguments.
+
+Regarding our teardown method, Ruby can be a bit lenient when it comes to closing files. If we didn't call 
+@file.close, then the File object associated with @file would be closed when our script is finished running. 
+This is a failsafe implemented by the IO class. But it is best to be explicit about these things. Since 
+this is an example where we only work with one file, things may turn out ok. But imagine if we were dealing 
+with several files, we would want to keep track of when they are opened and closed. It may not seem all 
+that intuitive that our File really is closed properly. We could add one more line of code to our test 
+file to verify that teardown is called after each test. We'll use the predicate method, closed? to verify 
+that we have closed our file.
+
+class TextTest < Minitest::Test
+  def setup
+    @file = File.open('./sample_text.txt', 'r')
+  end
+
+  # omitted for brevity
+
+  def test_word_count
+    text = Text.new(@file.read)
+    assert_equal 72, text.word_count
+  end
+
+  def teardown
+    @file.close
+    puts "File has been closed: #{@file.closed?}"
+  end
+end
+
+When we run our test, we should now see 2 lines of output(along with our test output), one line of output 
+for each test we have run in this file.
 =end
