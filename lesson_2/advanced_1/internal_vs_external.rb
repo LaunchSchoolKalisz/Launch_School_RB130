f980e9df043e7fca84d205316b3ee3a11b8cfb86
mskalisz <mskalisz3@gmail.com>
2022-02-05 10:35:19 -0500

    Add LS solution for internal vs external iterators

--- lesson_2/advanced_1/internal_vs_external.rb
+++ lesson_2/advanced_1/internal_vs_external.rb
@@ -56,13 +56,13 @@ An Enumerator also implements the Enumerable module. Keep this in mind, as it ma
 exercise easier to write.
 =end
 
-array = [1, 2, 3, 4, 5, 6, 7]
-num = array.size
-enumerator = array.each
+# array = [1, 2, 3, 4, 5, 6, 7]
+# num = array.size
+# enumerator = array.each
 
-enumerator.each_with_index do |element, idx|
-  result = 1
-end
+# enumerator.each_with_index do |element, idx|
+#   result = 1
+# end
 
 factorial = Enumerator.new do |to_yield|
   result = 1
@@ -86,3 +86,99 @@ factorial.each_with_index do |value, number|
   puts "#{number}! == #{value}"
   break if number >= 5
 end
+
+=begin
+LS Solution
+
+factorial = Enumerator.new do |yielder|
+  accumulator = 1
+  number = 0
+  loop do
+    accumulator = number.zero? ? 1 : accumulator * number
+    yielder << accumulator
+    number += 1
+  end
+end
+
+# External iterators
+
+6.times { |number| puts "#{number}! == #{factorial.next}" }
+puts "=========================="
+6.times { |number| puts "#{number}! == #{factorial.next}" }
+puts "=========================="
+factorial.rewind
+6.times { |number| puts "#{number}! == #{factorial.next}" }
+puts "=========================="
+
+# Internal iterators
+
+factorial.each_with_index do |value, number|
+  puts "#{number}! == #{value}"
+  break if number >= 5
+end
+Output:
+
+Copy Code
+0! == 1         # Output of first times loop
+1! == 1
+2! == 2
+3! == 6
+4! == 24
+5! == 120
+==========================
+0! == 720       # Output of 2nd times loop: note the incorrect output
+1! == 5040
+2! == 40320
+3! == 362880
+4! == 3628800
+5! == 39916800
+==========================
+0! == 1         # Output of 3rd times loop: this time output is correct
+1! == 1
+2! == 2
+3! == 6
+4! == 24
+5! == 120
+==========================
+0! == 1         # Output of each_with_index
+1! == 1
+2! == 2
+3! == 6
+4! == 24
+5! == 120
+
+Discussion
+Most of the work for solving this problem relates to creating our new Enumerator object. We can get a good 
+start at understanding how to do this by referencing the Enumerator::new method. It shows how to define a 
+new Enumerator that iterates over Fibonacci numbers. We'll start with that as a template, and alter it to 
+iterate over factorials instead. For iterating over factorials, we'll need two variables: one to track 
+which number factorial we are on, and another store the cumulative value of all prior factorials. We use 
+accumulator for the latter, and we use number for the former.
+
+When defining an external iterator, we set the next value in the iteration by calling yielder << value. In 
+this case, we use accumulator for value; it sets the current factorial value.
+
+To use our Enumerator object as an internal and external iterator, we utilize two different groups of code. 
+First, we have 3 calls to the Intger#times method; this code uses an external iterator (factorial.next). 
+Lastly, we have an #each_with_index loop; this code uses an internal iterator.
+
+There is some curious behavior with the external iterators -- the Enumerator doesn't know when to quit. On 
+the first call to #times, everything works as expected. However, in the second, things seem to have gotten 
+out of hand. The call to #times is not printing the desired values.
+
+The problem here is that the Enumerator doesn't know when we're done iterating when we using an external 
+iterator. That's something internal iterators do. However, with external iterators, we need to explicitly 
+tell the Enumerator when we're done. We do that by calling the Enumerator#rewind method, which we do on 
+line 15. Note that #rewind doesn't tell the Enumerator when to stop iterating; it really tells the 
+Enumerator that it's time to begin a new iteration.
+
+Once we call #rewind, the 3rd invocation of times again produces the correct results.
+
+Next, we move on to our #each_with_index call. #each_with_index is designed to work with Enumerator objects, 
+so it uses an internal iterator. In particular, #each_with_index knows that it needs to call #rewind each 
+time you invoke it. Therefore, when we invoke #each_with_index here, it automatically calls #rewind, and 
+outputs the desired results.
+
+Note that we need to explicitly break out of this loop; the factorial method doesn't have a natural stopping 
+point, but will keep going for as long as you let it.
+=end
