23c9ad289486c9070f8ba1a8bb97bdf139dc036c
mskalisz <mskalisz3@gmail.com>
2022-03-26 12:46:34 -0400

    Redo passing parameters part 1

--- study_guide/practice.rb
+++ study_guide/practice.rb
@@ -1,52 +1,24 @@
 =begin
-Below we have a listening device. It lets us record something that is said and store it for later use. 
-In general, this is how the device should be used:
+Passing Parameters Part 1
 
-Listen for something, and if anything is said, record it for later use. If nothing is said, then do not 
-record anything.
+Modify the method below so that the display/output of items is moved to a block, and its implementation 
+is left up to the user of the gather method.
 
-class Device
-  def initialize
-    @recordings = []
-  end
+items = ['apples', 'corn', 'cabbage', 'wheat']
 
-  def record(recording)
-    @recordings << recording
-  end
+def gather(items)
+  puts "Let's start gathering food."
+  puts "#{items.join(', ')}"
+  puts "Nice selection of food we have gathered!"
 end
-
-Anything that is said is retrieved by this listening device via a block. If nothing is said, then no 
-block will be passed in. The listening device can also output the most recent recording using a 
-Device#play method.
-
-listener = Device.new
-listener.listen { "Hello World!" }
-listener.listen
-listener.play # Outputs "Hello World!"
-Finish the above program so that the specifications listed above are met.
 =end
 
-class Device
-  def initialize
-    @recordings = []
-  end
+items = ['apples', 'corn', 'cabbage', 'wheat']
 
-  def record(recording)
-    @recordings << recording
-  end
-
-  def listen
-    if block_given?
-      record(yield)
-    end
-  end
-
-  def play
-    puts @recordings.last
-  end
+def gather(items)
+  puts "Let's start gathering food."
+  yield(items)
+  puts "Nice selection of food we have gathered!"
 end
 
-listener = Device.new
-listener.listen { "Hello World!" }
-listener.listen
-listener.play # Outputs "Hello World!"
+gather(items) {puts items.join(', ')}
